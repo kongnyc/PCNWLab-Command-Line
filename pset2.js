056b88884cef8c6613282b92f6da85186bfcb740
kongnyc <54258089+kongnyc@users.noreply.github.com>
2019-09-25 23:54:34 -0400

    finished  problemset 2

--- pset2.js
+++ pset2.js
@@ -13,25 +13,35 @@ In this problem, you will be downloading some files from online, then creating a
         /images
             amazon.jpg
             facebook.png
-            move facebook.png c:\Users\Yang\Blackfolder\PCNWLab-Command-Line\assets\images
+            move facebook.png c:\Users\Yang\Blackfolder\PCNWLab-Command-Line\assets\images //window cmd 
             google.jpg
             twitter.png
+            mv twitter.png cd ../assets/images/ //Mac cmd
         /memes
             meme.jpg
+            mv meme.jpg cd ../assets/memes/
         /src
             foo.js
             app.js
             hello.html
+               mv foo.js cd ../assets/src/
+                mv app.js cd ../assets/src/
+                 mv hello.html cd ../assets/src/
 
 4. Open `hello.html` using a code editor and change "SOME_CLASS" to "Full Stack: Nights & Weekends". Save it.
-
+code hello.html
+type "FSNW" in plcae "SC"
 5. You should have the following commits:
 - git initialized
+cd assets
+git init
 - added app.js
+git add .
 - added hello.html
 - created assets directory
 - added all images & memes
 - added all sources
 - modified hello file
+code src/hello.html
 ******************/
 
